b9cf97670ed7cdbe3eb430cd2bcf9db81ea82da5
Muthyala <2159799@cognizant.com>
2024-07-12 18:26:39 +0530

    adding challanges

---
+++ challange_1.js
@@ -0,0 +1,25 @@
+
+//DATA_1
+const massmark = 78;
+const heightmark = 1.69;
+const massjohn = 92;
+const heightjohn = 1.95;
+
+const BMImark= massmark/(heightmark*heightmark);
+const BMIjohn= massjohn/(heightjohn*heightjohn);
+
+const heigherBMI = BMImark > BMIjohn;
+console.log(BMIjohn,BMImark);
+
+//Data_2
+const massmark = 95;
+const heightmark =1.88;
+const massjohn =85;
+const heightjohn =1.76;
+
+const BMImark = massmark / (heightmark * heightmark);
+const BMIjohn = massjohn / (heightjohn * heightjohn);
+const heigherBMI = BMImark > BMIjohn;
+console.log(BMIjohn,BMImark);
+
+
